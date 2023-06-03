ab3339fc32a4bc000c7c867ed6978367b45641b5
Rudenches <xzdara@yandex.ru>
2023-06-03 17:48:53 +0300

    module2-task1

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -1,6 +1,25 @@
 import {createElement} from '../render.js';
+import {getOffersByType} from '../mock/consts';
+import {destinations} from '../mock/destination';
+import {getDateYears, getItemFromItemsById} from '../util.js';
 
-function createEditFormTemplate() {
+function createOffersTemplate(offers, type) {
+  return getOffersByType(type).map((offer) => {
+    const isOfferChecked = offers.includes(offer.id) ? 'checked' : '';
+    return `
+    <div class="event__offer-selector">
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${isOfferChecked}>
+      <label class="event__offer-label" for="event-offer-luggage-1">
+        <span class="event__offer-title">${offer.title}</span>
+        &plus;&euro;&nbsp;
+        <span class="event__offer-price">${offer.price}</span>
+      </label>
+    </div>`;
+  }).join('');
+}
+
+function createEditFormTemplate(oneWaypoint) {
+  const itemDest = getItemFromItemsById(destinations, oneWaypoint.destination);
   return (
     `<li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
@@ -8,7 +27,7 @@ function createEditFormTemplate() {
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+          <img class="event__type-icon" width="17" height="17" src="${oneWaypoint.type}.png" alt="${oneWaypoint.type} icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -66,9 +85,9 @@ function createEditFormTemplate() {
 
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+        ${oneWaypoint.type}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${itemDest.name}" list="destination-list-1">
         <datalist id="destination-list-1">
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
@@ -78,10 +97,10 @@ function createEditFormTemplate() {
 
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-1">From</label>
-        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
+        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${getDateYears(oneWaypoint.dateFrom)}">
         &mdash;
         <label class="visually-hidden" for="event-end-time-1">To</label>
-        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${getDateYears(oneWaypoint.dateFrom)}">
       </div>
 
       <div class="event__field-group  event__field-group--price">
@@ -147,12 +166,13 @@ function createEditFormTemplate() {
               <span class="event__offer-price">40</span>
             </label>
           </div>
+          ${createOffersTemplate(oneWaypoint.offersIDs, oneWaypoint.type)}
         </div>
       </section>
 
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-        <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
+        <p class="event__destination-description">${itemDest.description}</p>
       </section>
     </section>
   </form>
@@ -161,8 +181,12 @@ function createEditFormTemplate() {
 }
 
 export default class EditingForm {
+  constructor({oneWaypoint}) {
+    this.oneWaypoint = oneWaypoint;
+  }
+
   getTemplate() {
-    return createEditFormTemplate();
+    return createEditFormTemplate(this.oneWaypoint);
   }
 
   getElement() {
